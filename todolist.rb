06ab8ccb1bef355b3e28b84b7305e25a27eb1749
Thameem <sdthameem@gmail.com>
2016-03-13 08:57:49 -0700

    Fixed 3 review comments

--- todolist.rb
+++ todolist.rb
@@ -2,7 +2,7 @@
 require 'date'
 
 class TodoList
-    attr_reader :title, :items 
+    attr_reader :title, :items ,:report_file
 # Initialize todo list with a title and no items
     def initialize(list_title)
 		@title = list_title
@@ -10,22 +10,21 @@ class TodoList
 	end
 
 #Writing the output to a file 
-   $report_file = File.new("report.txt", "w+")
-	
+		report_file = File.new("report.txt", "w+")
+
 # Creates a new Item and adds it to the array of Items
 	def add_item(new_item)
-	    id = @items.length + 1
-	    item_add = Item.new(new_item,id)
+	    item_add = Item.new(new_item)
 	    @items.push(item_add)   #Adds new item to array
     end
 
 #Prints the Title and calls method to print item details
-    def print_list
+    def print_list()
     	$report_file .puts '*****************************'
     	$report_file.puts "Title - #{@title}"
     	$report_file .puts '*****************************'
-	    @items.each do |value|
-	        value.print_items
+	    @items.each_with_index do |value,index|
+	        value.print_items(index+1)
 	    end
 	end    	
 
@@ -34,9 +33,6 @@ class TodoList
         $report_file.puts '*****************************'
         $report_file.puts "Item: #{@items[item_pos+1].description} is removed"   	
 	    @items.delete_at(item_pos+1)
-	    @items.each_with_index do |value,index| #To get new Item id after removing item
-	        value.update_item_id(index)
-	    end
     end
 
 #Method to update title
@@ -63,28 +59,27 @@ class TodoList
 #Calls Item method and checks for each item before updating the due date.
     def update_due_date(item_name,due_date)
     	@items.each do |value|
-    	if value.description == item_name # check each item against input item
-    		value.update_item_due_date(due_date)
-    		$report_file.puts "Item: #{item_name} due date updated to #{due_date}"
-    	end
+    	    if value.description == item_name # check each item against input item
+    		    value.update_item_due_date(due_date)
+    		    $report_file.puts "Item: #{item_name} due date updated to #{due_date}"
+    	    end
         end
     end
 end
 
 class Item
-	attr_reader :description , :completed_status ,:due_date , :report_file
+	attr_reader :description , :completed_status ,:due_date 
 
 # Initialize item with a description and marked as not complete
-	def initialize(input_description,id)
+	def initialize(input_description)
 		@description = input_description
 		@completed_status = false  #Initialize completed status to false
 		date = DateTime.now + 10   
 		@due_date = date.strftime("%m/%d/%Y") #Initialize due date current date + 10days
-		@item_id = id
 	end
 #Method to print Item details
-	def print_items
-    	$report_file.puts "Item ID  : #{@item_id}"
+	def print_items(item_id)
+    	$report_file.puts "Item ID  : #{item_id}"
     	$report_file.puts "Item     : #{@description}"
     	$report_file.puts "Status   : #{@completed_status}"
     	$report_file.puts "Due date : #{@due_date}"
@@ -101,10 +96,6 @@ class Item
 #Method to update Item due date
     def update_item_due_date(new_due_date)
     	@due_date = new_due_date
-    end
-#Method to update Item id 
-    def update_item_id(new_item_id)
-    	@item_id = new_item_id + 1
     end
 end
 
